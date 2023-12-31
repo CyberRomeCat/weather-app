cae7bc1afabb87251b18dc8aaa7c4a36bb9bd997
jenzie68 <jacecabalo@gmail.com>
2023-12-31 18:46:26 +0400

    change display feature when location has been input

--- src/UI.js
+++ src/UI.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 const changeDisplay = (() => {
   const changeLocation = (region) => {
     const city = document.getElementById("city-name");
@@ -5,8 +6,57 @@ const changeDisplay = (() => {
     city.textContent = region;
   };
 
+  const date = (currentDate) => {
+    const getDate = document.getElementById("date");
+    const replaceStr = currentDate.replaceAll("-", " ");
+    const str = replaceStr.split(" ");
+    getDate.textContent = `${str[1]}/${str[2]}/${str[0]}`;
+  };
+
+  const time = (currentTime) => {
+    const getTime = document.getElementById("time");
+    const replaceStr = currentTime.replaceAll("-", " ");
+    const str = replaceStr.split(" ");
+    getTime.textContent = str[3];
+  };
+
+  const temperature = (currentTemp) => {
+    const getTemp = document.getElementById("temperature");
+    const roundTemp = Math.round(currentTemp);
+    getTemp.textContent = `${roundTemp}°C`;
+  };
+
+  const sky = (currentSky) => {
+    const getSky = document.getElementById("sky");
+    getSky.textContent = currentSky;
+  };
+
+  const feelsLike = (currentTemp) => {
+    const getFeelsLike = document.getElementById("feels-like");
+    const roundTemp = Math.round(currentTemp);
+    getFeelsLike.textContent = `Feels like: ${roundTemp}°C`;
+  };
+
+  const humidity = (currentHumidity) => {
+    const getHumidity = document.getElementById("humidity");
+    getHumidity.textContent = `Humidity: ${currentHumidity}%`;
+  };
+
+  const wind = (currentWind) => {
+    const getWind = document.getElementById("wind");
+    const roundWind = Math.round(currentWind);
+    getWind.textContent = `Wind: ${roundWind}km/hr`;
+  };
+
   return {
     changeLocation,
+    date,
+    time,
+    temperature,
+    sky,
+    feelsLike,
+    humidity,
+    wind,
   };
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import "./style.css";
 import changeDisplay from "./UI";
 
-const getWeatherData = async (userLocation) => {
-  const str = userLocation;
+function upperCase(string) {
+  const str = string;
   const modStr = str[0].toUpperCase() + str.slice(1);
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=490370ad19b74d52b78123129233011&q=${modStr}`,
-    { mode: "cors" },
-  );
-  const weatherData = await response.json();
-  if (
-    weatherData.location.name === modStr ||
-    weatherData.location.region === modStr ||
-    weatherData.location.country === modStr
-  ) {
-    changeDisplay.changeLocation(modStr);
+  return modStr;
+}
+
+const getWeatherData = async (userLocation) => {
+  const location = upperCase(userLocation);
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=490370ad19b74d52b78123129233011&q=${location}`,
+      { mode: "cors" },
+    );
+    const weatherData = await response.json();
+    if (
+      weatherData.location.name === location ||
+      weatherData.location.region === location ||
+      weatherData.location.country === location
+    ) {
+      const error = document.getElementById("error");
+      error.style.display = "none";
+      changeDisplay.changeLocation(location);
+      changeDisplay.temperature(weatherData.current.temp_c);
+      changeDisplay.sky(weatherData.current.condition.text);
+      changeDisplay.feelsLike(weatherData.current.feelslike_c);
+      changeDisplay.humidity(weatherData.current.humidity);
+      changeDisplay.wind(weatherData.current.wind_kph);
+      changeDisplay.date(weatherData.location.localtime);
+      changeDisplay.time(weatherData.location.localtime);
+    }
+  } catch (err) {
+    const error = document.getElementById("error");
+    error.style.display = "block";
   }
-  console.log(weatherData);
 };
 
 function getUserLocation() {
